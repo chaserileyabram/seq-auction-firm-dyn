160d643cef44fa2420be17a4becd6cfb1296451a
Chase Abram <abram@uchicago.edu>
2023-10-25 09:26:15 -0500

    first files

---
+++ calibrate_model.jl
@@ -0,0 +1,30 @@
+# Chase Abram
+
+# Go to proper directory here
+
+##
+# Load packges needed, as well as model setup code
+
+##
+
+# Use row number of iteration to grab moments
+
+lp_drift = 
+lp_pareto = 
+e_to_u =
+switch_dwq =
+mcal = calibrate_model([lp_drift, lp_pareto, e_to_u, switch_dwq])
+
+##
+# Write parameters to spreadsheet
+
+# Other stuff to save, maybe in a batch-labelled folder?
+
+
+
+
+
+
+
+
+
